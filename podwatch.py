5582e54728a3b9c166e886f9e99f2dc0ac0b9256
Leon Albers <leon.albers@inovex.de>
2020-03-28 11:39:21 +0100

    Restructure and improvements

--- podwatch.py
+++ podwatch.py
@@ -1,34 +1,52 @@
+import subprocess
 import podman
 import requests
 import json
 import os
 
-# systemctl --user start io.podman.socket
+# Pre-Requirements: Podman.socket
+# > systemctl --user start io.podman.socket
 
 def get_registry_digest(image_name, registry, tag):
-    if registry == "docker.io":
-        token = "Bearer " + json.loads(requests.get(f"https://auth.docker.io/token?service=registry.docker.io&scope=repository:{image_name}:pull").text)['token']
-        digest = requests.head(f"https://registry-1.docker.io/v2/{image_name}/manifests/{tag}", headers={ "Authorization" : token}).headers["Docker-Content-Digest"]
-        return digest
-    else:
-        raise NotImplementedError(f"Registry `{registry}` not implement")
+    try:
+        if registry == "docker.io":
+            token = "Bearer " + json.loads(requests.get(f"https://auth.docker.io/token?service=registry.docker.io&scope=repository:{image_name}:pull").text)['token']
+            digest = requests.head(f"https://registry-1.docker.io/v2/{image_name}/manifests/{tag}", headers={ "Authorization" : token}).headers["Docker-Content-Digest"]
+            return digest
+        else:
+            raise NotImplementedError(f"Registry `{registry}` not implement")
+    except Exception as ex:
+        raise Exception(f"Could not query registry `{registry}`. Err: {str(ex)}")
 
+def main():
+    with podman.Client(uri=f"unix:/run/user/{os.getuid()}/podman/io.podman") as client:
+        updated_images = []
+        for image in client.images.list():
+            try:
+                # Extract registry, image_name and tag
+                registry = image.repoTags[0].split("/")[0]
+                image_name = "/".join(image.repoTags[0].split("/")[1:]).split(":")[0]
+                tag = image.repoTags[0].split(":")[1]
 
-with podman.Client(uri=f"unix:/run/user/{os.getuid()}/podman/io.podman") as client:
-    images = {}
-    for image in client.images.list():
-        try:
-            tag = image.repoTags[0].split(":")[1]
-            registry = image.repoTags[0].split("/")[0]
-            image_name = "/".join(image.repoTags[0].split("/")[1:]).split(":")[0]
-            upsteam_digest = get_registry_digest(image_name, registry, tag)
-            images[image.repoTags[0]] = { 'local' : image.digest, 'upstream' : upsteam_digest}
-            if upsteam_digest != image.digest:
-                print (f"Update Image `{image_name}` ({image.digest})")
-                image.update()
-                print (f"New Image Digest `{image_name}` ({image.digest})")
-        except:
-            pass
+                # Load current digest from registry
+                upsteam_digest = get_registry_digest(image_name, registry, tag)
 
-    for container in client.containers.list():
-        print(container)
+                # Compare and update image
+                if upsteam_digest != image.digest:
+                    print (f"Update Image `{image_name}` ({image.digest})")
+                    updated_images.append(image.repoTags[0])
+                    subprocess.call(["podman", "image", "pull", image.repoTags[0]])
+            except Exception as ex:
+                print(f"Could not check image {image.repoTags[0]}. Err: {str(ex)}")
+
+        # Restart containers which are using an old version of an updated image
+        for container in client.containers.list():
+            if container.image in updated_images:
+                if container.containerrunning:
+                    container.stop()
+                    print(f"Stopped pod.")
+                    container.start()
+                    print(f"Started pod.")
+
+if __name__ == "__main__":
+    main()
