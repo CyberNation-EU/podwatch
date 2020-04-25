c8b764f3fcd85346261f567b3f1fff8d5459f58e
Leon Albers <leon.albers@inovex.de>
2020-04-25 16:55:26 +0200

    Use application/vnd.docker.distribution.manifest.v2+json to get digest

--- podwatch.py
+++ podwatch.py
@@ -38,7 +38,7 @@ class Podwatch:
         try:
             if registry == "docker.io":
                 token = "Bearer " + json.loads(requests.get(f"https://auth.docker.io/token?service=registry.docker.io&scope=repository:{image_name}:pull").text)['token']
-                digest = requests.head(f"https://registry-1.docker.io/v2/{image_name}/manifests/{tag}", headers={ "Authorization" : token}).headers["Docker-Content-Digest"]
+                digest = json.loads(requests.get(f"https://registry-1.docker.io/v2/{image_name}/manifests/{tag}", headers={ "Authorization" : token, "Accept": "application/vnd.docker.distribution.manifest.v2+json"}).content)["config"]["digest"]
                 return digest
             else:
                 raise NotImplementedError(f"Registry `{registry}` not implement")
@@ -60,15 +60,16 @@ class Podwatch:
                             self.logger.debug(f"Ignoring Image from registry {registry}.")
                             continue
 
-                        self.logger.info(f"Validate Image: {registry}/{image_name}:{tag}.")
+                        self.logger.debug(f"Validate Image: {registry}/{image_name}:{tag}.")
 
                         # Load current digest from registry
-                        upsteam_digest = self.get_registry_digest(image_name, registry, tag)
-                        self.logger.info(f"Compare upstream digest. Local ({image.digest}). Remote ({upsteam_digest})")
+                        upsteam_digest = self.get_registry_digest(image_name, registry, tag)[7:]
+                        self.logger.debug(f"Compare upstream digest. Local ({image.id}). Remote ({upsteam_digest})")
 
                         # Compare and update image
-                        if upsteam_digest != image.digest:
+                        if upsteam_digest != image.id:
                             updated_images.append(image.repoTags[0])
+                            self.logger.info(f"Different digest pull image {image.repoTags[0]}")
                             if self.__args.dry_run:
                                 self.logger.info(f"Exec: podman image pull -q {image.repoTags[0]}")
                             else:
@@ -79,13 +80,17 @@ class Podwatch:
                     else:
                         self.logger.warning(f"Could not check image {image.id}. Err: {str(ex)}")
 
+            cnt_restart = 0
             # Restart containers which are using an old version of an updated image
             for container in client.containers.list():
                 if container.image in updated_images:
                     if container.containerrunning:
                         if not self.__args.dry_run:
                             container.restart()
+                        cnt_restart += 1
                         self.logger.info(f"Restarted container {container.names}.")
+
+            print(f"Updated {str(len(updated_images))} images. Restarted {str(cnt_restart)} associated containers")
 
 if __name__ == "__main__":
     podatch = Podwatch()
